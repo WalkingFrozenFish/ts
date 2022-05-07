f46823a4655990aa45743fc486c76c2e64ddf523
WalkingFrozenFish <chachin.ol@yandex.kz>
2022-05-07 20:25:17 +0600

    Загрузка картинок

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import "./App.css"
 
-function App() {
+const App = () => {
+  const [photos, setPhotos] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [fetching, setFetching] = useState(true)
+  const [totalCount, setTotalCount] = useState(0)
+
+  useEffect(() => {
+    if (fetching) {
+      console.log("fetching")
+      axios.get(`https://jsonplaceholder.typicode.com/photos?_limit=10&_page=${currentPage}`)
+        .then(response => {
+          setPhotos([...photos, ...response.data])
+          setCurrentPage(prevState => prevState + 1)
+          setTotalCount(response.headers['x-total-count'])
+        })
+        .finally(() => {
+          setFetching(false)
+        })
+    }
+  }, [fetching])
+
+  useEffect(() => {
+    document.addEventListener("scroll", scrollHandler)
+    return function () {
+      document.removeEventListener("scroll", scrollHandler)
+    }
+  }, [totalCount])
+
+  const scrollHandler = (e) => {
+    if ((e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100) && (photos.length < totalCount)) {
+      console.log("Scroll")
+      setFetching(true)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"app"}>
+      {photos.map(photo => {
+        return (<div className="photo" key={photo.id}>
+          <div className="title">{photo.id}. {photo.title}</div>
+          <img src={photo.url} alt="" />
+        </div>)
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
